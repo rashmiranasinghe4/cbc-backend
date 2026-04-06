3b4471e593a01d5ba0ad8d55989c42e5b2567ca0
Rashmi Ranasinghe <rpranasinghe1104@gmail.com>
2026-04-06 23:53:07 +0530

    Implemented Google login functionality

--- routers/userRouters.js
+++ routers/userRouters.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createUser, loginUser ,getUser } from '../controllers/userController.js';
+import { createUser, loginUser ,getUser , googleLogin } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 userRouter.post("/",createUser)
 userRouter.get("/",getUser)
 userRouter.post("/login",loginUser)
+userRouter.post("/google-login", googleLogin)
 
 export default userRouter;
